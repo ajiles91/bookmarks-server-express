f78343c9f2c1b24b7bc2f530f79f2182d421b442
Amber <ajiles91@gmail.com>
2019-09-04 21:01:51 -0400

    sub for Checkpoint 17

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const { isWebUri } = require('valid-url')
 const logger = require('../logger')
-
+const { getBookmarkValidationError } = require('./bookmark-validator')
 const BookmarksService = require('./bookmarks-service')
 const xss = require('xss')
-
+const path = require('path')
 const bookmarksRouter = express.Router()
 const bodyParser = express.json()
 
@@ -17,7 +17,8 @@ const serializeBookmark = bookmark => ({
 })
 
 bookmarksRouter
-  .route('/bookmarks')
+  .route('/')
+
   .get((req, res, next) => {
     BookmarksService.getAllBookmarks(req.app.get('db'))
       .then(bookmarks => {
@@ -25,49 +26,42 @@ bookmarksRouter
       })
       .catch(next)
   })
+
   .post(bodyParser, (req, res, next) => {
-    // TODO: update to use db
+    const { title, url, description, rating } = req.body
+    const newBookmark = { title, url, description, rating }
+
     for (const field of ['title', 'url', 'rating']) {
-      if (!req.body[field]) {
+      if (!newBookmark[field]) {
         logger.error(`${field} is required`)
         return res.status(400).send({
           error: { message: `'${field}' is required` }
-      })
+        })
       }
     }
-    const { title, url, description, rating } = req.body
 
-    if (!Number.isInteger(rating) || rating < 0 || rating > 5) {
-      logger.error(`Invalid rating '${rating}' supplied`)
-      return res.status(400).send({
-      error: {message:`'rating' must be a number between 0 and 5`}
-      })
-    }
+    const error = getBookmarkValidationError(newBookmark)
 
-    if (!isWebUri(url)) {
-      logger.error(`Invalid url '${url}' supplied`)
-      return res.status(400).send({
-      error: {message:`'url' must be a valid URL`}
-      })
-    }
-
-    const newBookmark = { id: uuid(), title, url, description, rating }
+    if (error) return res.status(400).send(error)
 
     BookmarksService.insertBookmark(
-      req.app.get('db'), newBookmark
+      req.app.get('db'),
+      newBookmark
     )
-    .then(bookmark => {
-      logger.info(`Bookmark with id ${bookmark.id} created`)
-      res
-        .status(201)
-        .location(`/bookmarks/${bookmark.id}`)
-        .json(serializeBookmark(bookmark))
-    })
-    .catch(next)
+      .then(bookmark => {
+        logger.info(`Bookmark with id ${bookmark.id} created.`)
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `${bookmark.id}`))
+          .json(serializeBookmark(bookmark))
+      })
+      .catch(next)
   })
 
+
 bookmarksRouter
-  .route('/bookmarks/:bookmark_id')
+  .route('/:bookmark_id')
+
   .all((req, res, next) => {
     const { bookmark_id } = req.params
     BookmarksService.getById(req.app.get('db'), bookmark_id)
@@ -78,26 +72,58 @@ bookmarksRouter
             error: { message: `Bookmark Not Found` }
           })
         }
+
         res.bookmark = bookmark
         next()
       })
       .catch(next)
+
   })
-  .get((req,res) => {
+
+  .get((req, res) => {
     res.json(serializeBookmark(res.bookmark))
   })
+
   .delete((req, res, next) => {
-      const { bookmark_id } = req.params
-      BookmarksService.deleteBookmark(
-        req.app.get('db'),
-        bookmark_id
-      )
+    const { bookmark_id } = req.params
+    BookmarksService.deleteBookmark(
+      req.app.get('db'),
+      bookmark_id
+    )
       .then(numRowsAffected => {
         logger.info(`Bookmark with id ${bookmark_id} deleted.`)
         res.status(204).end()
       })
       .catch(next)
-})
-    
+  })
+
+  .patch(bodyParser, (req, res, next) => {
+    const { title, url, description, rating } = req.body
+    const bookmarkToUpdate = { title, url, description, rating }
+
+    const numberOfValues = Object.values(bookmarkToUpdate).filter(Boolean).length
+    if (numberOfValues === 0) {
+      logger.error(`Invalid update without required fields`)
+      return res.status(400).json({
+        error: {
+          message: `Request body must content either 'title', 'url', 'description' or 'rating'`
+        }
+      })
+    }
+
+    const error = getBookmarkValidationError(bookmarkToUpdate)
+
+    if (error) return res.status(400).send(error)
+
+    BookmarksService.updateBookmark(
+      req.app.get('db'),
+      req.params.bookmark_id,
+      bookmarkToUpdate
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
 
 module.exports = bookmarksRouter
